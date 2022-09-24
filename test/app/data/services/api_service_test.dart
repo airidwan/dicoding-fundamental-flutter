c57412f7ff6c3bd62b2414b81b0c9b43b57932ed
ridwanwong <a.ridwanudin@outlook.com>
2022-09-24 14:04:26 +0700

    comment mockito

--- test/app/data/services/api_service_test.dart
+++ test/app/data/services/api_service_test.dart
@@ -4,22 +4,22 @@ import 'package:http_mock_adapter/http_mock_adapter.dart';
 import 'package:submission_restaurantapp/app/data/constants/api_keys.dart';
 import 'package:submission_restaurantapp/app/data/model/index.dart';
 import 'package:submission_restaurantapp/app/data/services/api_services.dart';
-import 'package:mockito/mockito.dart';
-import 'package:mockito/annotations.dart';
+// import 'package:mockito/mockito.dart';
+// import 'package:mockito/annotations.dart';
 
 import 'test_constant.dart';
 
-import 'api_service_test.mocks.dart';
+// import 'api_service_test.mocks.dart';
 
-@GenerateMocks([ApiServices])
+// @GenerateMocks([ApiServices])
 void main() {
   late Dio _dio;
   late DioAdapter _dioAdapter;
   const _restaurantId = 'rqdv5juczeskfw1e867';
-  late MockApiServices apiServiceMock;
+  // late MockApiServices apiServiceMock;
 
   setUp(() {
-    apiServiceMock = MockApiServices();
+    // apiServiceMock = MockApiServices();
     _dio = Dio()
       ..options.baseUrl = ApiKeys.baseUrl
       ..options.connectTimeout = 25000
@@ -32,13 +32,13 @@ void main() {
   });
 
   group('test APi Service', () {
-    test('return apiresult', () async {
-      final apiResult = ApiresultModel<RestaurantModel>(error: false, message: 'success', data: RestaurantModel(id: _restaurantId));
-      when(apiServiceMock.getRestaurantDetailById(_restaurantId)).thenAnswer((_) async => await Future.value(apiResult));
-      final _result = await apiServiceMock.getRestaurantDetailById(_restaurantId);
-      expect(_result, isA<ApiresultModel<RestaurantModel>>());
-      expect(_result.error, equals(apiResult.error));
-    });
+    // test('return apiresult', () async {
+    //   final apiResult = ApiresultModel<RestaurantModel>(error: false, message: 'success', data: RestaurantModel(id: _restaurantId));
+    //   when(apiServiceMock.getRestaurantDetailById(_restaurantId)).thenAnswer((_) async => await Future.value(apiResult));
+    //   final _result = await apiServiceMock.getRestaurantDetailById(_restaurantId);
+    //   expect(_result, isA<ApiresultModel<RestaurantModel>>());
+    //   expect(_result.error, equals(apiResult.error));
+    // });
 
     test(
       'Get restaurant detail by Id',
