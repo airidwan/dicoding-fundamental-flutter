0bc24b05f31d886ebc405216afa446ce7cfed0d6
ridwanwong <a.ridwanudin@outlook.com>
2022-09-25 09:55:13 +0700

    remove coment

--- lib/app/utils/date_time_helper.dart
+++ lib/app/utils/date_time_helper.dart
@@ -3,7 +3,6 @@ import 'package:intl/intl.dart';
 
 class DateTimeHelper {
   static DateTime format(DateTime now) {
-    // final now = DateTime.now();
     final dateFormat = DateFormat('y/M/d');
     const timeSpecific = "11:00:00";
     final completeFormat = DateFormat('y/M/d H:m:s');
